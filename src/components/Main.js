93e3831982e37d3bf97eb20e919bba52390d2d35
Maria <bc-macair023@BCmacair023sAir.wlan>
2021-12-13 10:12:32 +0200

    screenshot

---
+++ src/components/Main.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+
+class Main extends Component {
+  state = {
+    counter: 0,
+  };
+
+  addOneHandler = () => {
+    this.setState({ counter: this.state.counter + 1 });
+  };
+
+  removeOneHandler = () => {
+    if (this.state.counter !== 0) {
+      this.setState({ counter: this.state.counter - 1 });
+    }
+  };
+
+  resetHandler = () => {
+    this.setState({ counter: 0 });
+  };
+
+  render() {
+    let circleClass = `${
+      this.state.counter === 0
+        ? ""
+        : this.state.counter % 2 === 0
+        ? "even"
+        : "odd"
+    } circle`;
+
+    return (
+      <main>
+        <div>
+          <div className={circleClass}>
+            <h1>{this.state.counter}</h1>
+          </div>
+          <button onClick={this.addOneHandler}>Add one</button>
+          <button onClick={this.removeOneHandler}>Remove one</button>
+          <button onClick={this.resetHandler}>Reset</button>
+        </div>
+      </main>
+    );
+  }
+}
+
+export default Main;
